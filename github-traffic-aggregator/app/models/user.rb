f0613c4df7e5d12519e67ce8a583db5ebd960f35
Jeremy Watt <jermwatt@gmail.com>
2025-01-11 19:51:46 -0700

    user and repo models added, gems re-installed

---
+++ github-traffic-aggregator/app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  # has_secure_password - requires uncommenting '''gem "bcrypt", "~> 3.1.7"''' in Gemfile
+  has_many :repos, dependent: :destroy
+
+end
---
+++ github-traffic-aggregator/db/migrate/20250112024759_create_repos.rb
@@ -0,0 +1,11 @@
+class CreateRepos < ActiveRecord::Migration[8.0]
+  def change
+    create_table :repos do |t|
+      t.string :name
+      t.text :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
