8c445c9ce8cf96d6d6aa25b6e7956bb09d3eb8bd
Jeremy Watt <jermwatt@gmail.com>
2025-01-25 11:29:59 -0700

    stimulus controller for datepicker, debugger test

--- github-traffic-aggregator/app/controllers/repos_controller.rb
+++ github-traffic-aggregator/app/controllers/repos_controller.rb
@@ -5,13 +5,28 @@ class ReposController < ApplicationController
   end
 
   def show
-    # Get the repo_data
+    # get repo id
     @repo = Repo.find(params[:id])
-    @repo_data_traffic_data_views_daily = RepoData::RepoTrafficDataViewsDaily.find_by({repo: Repo.find(params[:id])})
-    @repo_data_traffic_data_views_biweekly = RepoData::RepoTrafficDataViewsBiweekly.find_by({repo: Repo.find(params[:id])})
-    @repo_data_traffic_data_clones_daily = RepoData::RepoTrafficDataClonesDaily.find_by({repo: Repo.find(params[:id])})
-    @repo_data_traffic_data_clones_biweekly = RepoData::RepoTrafficDataClonesBiweekly.find_by({repo: Repo.find(params[:id])})
-    @repo_data_traffic_data_referrers_weekly = RepoData::RepoTrafficDataReferrersWeekly.find_by({repo: Repo.find(params[:id])})
-    @repo_data_traffic_data_paths_weekly = RepoData::RepoTrafficDataPathsWeekly.find_by({repo: Repo.find(params[:id])})
+
+    # set time range #STOPPED HERE
+    default_start_time = 
+
+    # get the repo id and time period returned from the form
+    if params[:start_date_between]
+
+
+    else
+  
+      # Get the repo_data for default time period - previous 14 days
+      
+
+
+      @repo_data_traffic_data_views_daily = RepoData::RepoTrafficDataViewsDaily.find_by({repo: Repo.find(params[:id])})
+      @repo_data_traffic_data_views_biweekly = RepoData::RepoTrafficDataViewsBiweekly.find_by({repo: Repo.find(params[:id])})
+      @repo_data_traffic_data_clones_daily = RepoData::RepoTrafficDataClonesDaily.find_by({repo: Repo.find(params[:id])})
+      @repo_data_traffic_data_clones_biweekly = RepoData::RepoTrafficDataClonesBiweekly.find_by({repo: Repo.find(params[:id])})
+      @repo_data_traffic_data_referrers_weekly = RepoData::RepoTrafficDataReferrersWeekly.find_by({repo: Repo.find(params[:id])})
+      @repo_data_traffic_data_paths_weekly = RepoData::RepoTrafficDataPathsWeekly.find_by({repo: Repo.find(params[:id])})
+    end
   end
 end
