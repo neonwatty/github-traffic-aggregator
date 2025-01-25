daf45411b1b73737017e2cd77c7705ddb53a3649
Jeremy Watt <jermwatt@gmail.com>
2025-01-25 10:50:40 -0700

    daterangepicker moved to stimulus

--- github-traffic-aggregator/app/javascript/controllers/daterangepicker_controller.js
+++ github-traffic-aggregator/app/javascript/controllers/daterangepicker_controller.js
@@ -1,7 +1,8 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="daterangepicker"
 export default class extends Controller {
   connect() {
+    new DateRangePicker(this.element);
   }
 }
